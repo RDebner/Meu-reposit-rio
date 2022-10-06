f061f6bafdf18036ad71e3289cb1426d9321fde0
Rafael <rafaeldebner08@gmail.com>
2022-10-06 20:06:53 -0300

    Mensagem de aniversário :)

--- primeirocodigo.js
+++ primeirocodigo.js
@@ -1,6 +1,6 @@
-console.log("Olá mundo!")
+let bomdia ="Bom dia Rafael Pereira Debner!"
 let ano = 2022
-let dia = 06
-let mês = "outubro"
-let frase = ` neste dia ${dia} de ${mês} de ${ano} queria desejar a todos um ótimo dia `
+let dia = 04
+let mês = "novembro"
+let frase = ` ${bomdia} Neste dia ${dia} de ${mês} de ${ano} queria desejar a você um feliz aniversário, tudo de bom para você e um ótimo dia.`
 console.log(frase)
