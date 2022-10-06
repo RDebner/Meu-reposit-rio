033c1a05870e56ccbb597e989ccb4c5a056049b4
Rafael <rafaeldebner08@gmail.com>
2022-10-06 20:12:43 -0300

    Mensagem de bom dia

---
+++ segundocodigo.js
@@ -0,0 +1,7 @@
+let dia = 06
+let mês = 'outubro'
+let ano = 2022
+let nome = 'Rafael'
+let bomdia = ` Bom dia ${nome}! Hoje é dia ${dia} de ${mês} de ${ano}`
+let mensagem = bomdia
+console.log(mensagem)
