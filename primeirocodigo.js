82d5ae085c45f65bc73891cb8573adf8cadb5fb9
Rafael <rafaeldebner08@gmail.com>
2022-10-06 19:58:13 -0300

    Mensagem de bom dia

--- primeirocodigo.js
+++ primeirocodigo.js
@@ -1,1 +1,6 @@
 console.log("Olá mundo!")
+let ano = 2022
+let dia = 06
+let mês = "outubro"
+let frase = ` neste dia ${dia} de ${mês} de ${ano} queria desejar a todos um ótimo dia `
+console.log(frase)
